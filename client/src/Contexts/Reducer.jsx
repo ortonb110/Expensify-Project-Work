547ee280ff50e1520e6addeed655f13d8867ede0
Orton Bright <ortonb110@gmail.com>
2023-06-26 01:09:42 +0000

    Use Reducer Setup

---
+++ client/src/Contexts/Reducer.jsx
@@ -0,0 +1,27 @@
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./Action";
+
+
+
+const reducer = (state, action) => {
+    if(action.type === DISPLAY_ALERT) {
+      return  {
+            ...state,
+            showAlert: true,
+            alertType: 'danger',
+            alertText: 'Please provide all values!'
+        }
+    }
+    if(action.type === CLEAR_ALERT) {
+        return {
+            ...state,
+            showAlert: false,
+            alertText: '',
+            alertType: ''
+        }
+    }
+
+    throw new Error(`no such action: ${action.type}`);
+}
+
+
+export default reducer;
